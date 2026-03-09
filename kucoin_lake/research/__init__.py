0dbb61a9d1f1f1b7428609bc1732a443416b38fa
Ben Church <benchurch@Bens-Mac-Studio.cable.virginm.net>
2026-03-09 21:34:37 +0000

    implemented panel creation and docs

--- kucoin_lake/research/__init__.py
+++ kucoin_lake/research/__init__.py
@@ -4,11 +4,14 @@ from __future__ import annotations
 
 __all__ = [
     "build_universe_daily",
+    "build_base_panel",
     "load_candidate_ranks",
     "apply_hysteresis",
     "summarize_universe",
+    "summarize_panel",
 ]
 
+from .panel import build_base_panel, summarize_panel
 from .universe import (
     apply_hysteresis,
     build_universe_daily,
